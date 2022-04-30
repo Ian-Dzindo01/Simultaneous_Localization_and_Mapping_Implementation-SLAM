57dd3070b7063298c327aaabd796ac3a360a18f7
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-30 20:04:00 +0200

    Marking keypoints

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import cv2
-import pygame
 import time
 from display import Display
 
@@ -9,9 +8,16 @@ W = 1920//2
 H = 1080//2
 
 display = Display(W, H)
+orb = cv2.ORB_create()
 
 def process_image(img):
     img = cv2.resize(img, (W,H))
+
+    kp, des = orb.detectAndCompute(img, None)        #keypoints and descriptors
+    for p in kp:
+        u, v = map(lambda x: int(round(x)), p.pt)
+        cv2.circle(img, (u,v), color=(0,255,0), radius=3)
+
     display.draw(img)
 
 if __name__ == "__main__":
