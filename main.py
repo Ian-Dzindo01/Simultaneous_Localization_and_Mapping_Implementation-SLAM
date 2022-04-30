3413ab520f479b2ec3b1a44a57b1238ad40ac474
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-30 16:37:55 +0200

    Display working correctly

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ import cv2
 import pygame
 import time
 import sdl2.ext
+
 W = 1920//2
 H = 1080//2
 
-screen = pygame.display.set_mode((W,H))
+pygame.init()
+display = pygame.display.set_mode((W,H))
+surface = pygame.Surface((W,H)).convert()
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-
-def process_image(img):
-    img = cv2.resize(img, (1920//2, 1080//2))
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    pygame.surfarray.blit_array(surface, img.swapaxes(0,1))
+    display.blit(surface, (0,0))
+    pygame.display.update()
     pygame.display.flip()
-    # cv2.imshow('image', img)
-    print(img.shape)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("test1.mp4")
 
     while cap.isOpened():
         ret, frame = cap.read()
-        # print(ret)
+
         if ret == True:
-            process_image(frame)
+            process_frame(frame)
         else:
             break
 
