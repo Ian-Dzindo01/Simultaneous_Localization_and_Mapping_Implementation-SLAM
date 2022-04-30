455fba586d5784a27c4e139c5bec0cbaa608782b
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-30 17:49:59 +0200

    Switched to SDL2

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ import pygame
 import time
 import sdl2.ext
 
+sdl2.ext.init()
+
 W = 1920//2
 H = 1080//2
 
-pygame.init()
-display = pygame.display.set_mode((W,H))
-surface = pygame.Surface((W,H)).convert()
-
 def process_frame(img):
     img = cv2.resize(img, (W, H))
-    pygame.surfarray.blit_array(surface, img.swapaxes(0,1))
-    display.blit(surface, (0,0))
-    pygame.display.update()
-    pygame.display.flip()
+    events = sdl2.ext.get_events()
+    cv2.imshow('image', img)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("test1.mp4")
