b053457c0103c9d73e9e6f71c26ea2cd9d35c80a
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-04-30 19:05:05 +0200

    Color problem fixed

--- display.py
+++ display.py
@@ -15,8 +15,8 @@ class Display(object):
             if event.type == sdl2.SDL_QUIT:
                 exit(0)
 
-        surf = sdl2.ext.pixels2d(self.window.get_surface())
-        surf[:] = img.swapaxes(0,1)[:, :, 0]
+        surf = sdl2.ext.pixels3d(self.window.get_surface())
+        surf[:,:,0:3] = img.swapaxes(0,1)
 
         self.window.refresh()
 
