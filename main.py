88fb98db0dfb0eae1b6146d4cae9d56ff71b25f4
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-05-01 15:01:26 +0200

    Implemented simple feature extractor

--- main.py
+++ main.py
@@ -8,12 +8,39 @@ W = 1920//2
 H = 1080//2
 
 display = Display(W, H)
-orb = cv2.ORB_create()
+
+class FeatureExtractor(object):
+    GX = 16//2
+    GY = 12//2
+
+    def __init__(self):
+        self.orb = cv2.ORB_create(1000)   # 1000 nfeatures
+
+    def extract(self, img):
+        # run detect in grid
+        sy = img.shape[0]//self.GX
+        sx = img.shape[1]//self.GX
+
+        akp = []
+        for ry in range(0, img.shape[0], sy):
+            for rx in range(0, img.shape[1], sx):
+                img_block = img[ry:ry+sy, rx:rx+sx]           # working in a grid range
+                kp = self.orb.detect(img_block, None)         # keypoints and descriptors
+
+                for p in kp:
+                    p.pt = (p.pt[0] + rx, p.pt[1] + ry)
+                    akp.append(p)
+
+        return akp
+
+
+
+fe = FeatureExtractor()
 
 def process_image(img):
     img = cv2.resize(img, (W,H))
+    kp = fe.extract(img)
 
-    kp, des = orb.detectAndCompute(img, None)        #keypoints and descriptors
     for p in kp:
         u, v = map(lambda x: int(round(x)), p.pt)
         cv2.circle(img, (u,v), color=(0,255,0), radius=3)
