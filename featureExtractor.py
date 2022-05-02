4dd10b1b2ec83b467c46fed3fbaa46da4e6fac2c
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2022-05-02 16:03:23 +0200

    Minor changes

--- featureExtractor.py
+++ featureExtractor.py
@@ -14,17 +14,21 @@ class FeatureExtractor(object):
     def extract(self, img):
         # detection
         feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)    # strong corners on an image
-
+                                                                                                                          # image needs to be black and white
         # extraction
-        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]
-        kps, des = self.orb.compute(img, kps)
+        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]           # extract keypoints from features
+        kps, des = self.orb.compute(img, kps)                                        # compute keypoints and descriptors
 
         # matching
-        matches = None
+        matches = []
         if self.last is not None:
-            matches = self.bf.match(des, self.last['des'])
-            matches = zip([kps[m.queryIdx] for m in matches], [self.last['kps'][m.trainIdx] for m in matches])
+            matches = self.bf.knnMatch(des, self.last['des'])                         # using knn instead of regular match
+            matches = zip([kps[m.queryIdx] for m in matches], [self.last['kps'][m.trainIdx] for m in matches])    # query and training data
 
+        self.last = {'kps' : kps, 'des' : des}     # keypoints and descriptors
 
-        self.last = {'kps' : kps, 'des' : des}
         return matches
+
+
+# train image is the one learned, query is the image you are trying to match with the
+# ones already trained
--- main.py
+++ main.py
@@ -12,19 +12,17 @@ display = Display(W, H)
 fe = FeatureExtractor()
 
 def process_image(img):
-    img = cv2.resize(img, (W,H))
-    matches = fe.extract(img)
-    if matches is None:
-        return
+    img = cv2.resize(img, (W,H))    # image resize to W, H
+    matches = fe.extract(img)       # extract features wth FE
 
     for pt1, pt2 in matches:
-        u1,v1 = map(lambda x: int(round(x)), pt1.pt)
+        u1,v1 = map(lambda x: int(round(x)), pt1.pt)         # unzip match data
         u2,v2 = map(lambda x: int(round(x)), pt2.pt)
 
-        cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
-        cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
+        cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)      # draw circles aroung the matches
+        cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))        # draw lines in between
 
-    display.draw(img)
+    display.draw(img)        # display image
 
 
 if __name__ == "__main__":
